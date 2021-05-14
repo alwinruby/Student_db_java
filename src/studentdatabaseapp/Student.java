f2b23787b496616db4be6ef3bf011f599e1a901b
alwinruby <alwinsolanky@gmail.com>
2021-05-14 14:56:03 +0100

    Added outline

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,24 @@
 package studentdatabaseapp;
 
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String Courses;
+    private String Balance;
+
+
+    //Constructor: prompt user to enter a student's name and year
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View balance
+
+    //Pay tuition
+
+    //Show status
 }
--- src/studentdatabaseapp/StudentDataBaseApp.java
+++ src/studentdatabaseapp/StudentDataBaseApp.java
@@ -1,4 +1,9 @@
 package studentdatabaseapp;
 
 public class StudentDataBaseApp {
+    public static void main(String[] args){
+        //Ask how many users we want to add
+
+        //Create n number of students
+    }
 }
