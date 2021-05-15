8d75c1aaa7c5d101d407b3ec1f6a15d602e0e7d8
alwinruby <alwinsolanky@gmail.com>
2021-05-15 07:12:03 +0100

    Constructor started

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,17 +1,32 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
-    private String Courses;
-    private String Balance;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse;
 
 
     //Constructor: prompt user to enter a student's name and year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Students first name: ");
+        this.firstName= in.nextLine();
 
+        System.out.println("Enter Students last name: ");
+        this.lastName= in.nextLine();
+
+        System.out.println("1 - Freshman \n2 - Sophomore \n3 - Junior \n4 - Senior \nEnter Student class level : ");
+        this.gradeYear = in.nextInt();
+
+        System.out.println(firstName + " " + lastName + " - " +gradeYear);
+    }
     //Generate an ID
 
     //Enroll in courses
--- src/studentdatabaseapp/StudentDataBaseApp.java
+++ src/studentdatabaseapp/StudentDataBaseApp.java
@@ -2,6 +2,8 @@ package studentdatabaseapp;
 
 public class StudentDataBaseApp {
     public static void main(String[] args){
+
+        Student stu1 = new Student();
         //Ask how many users we want to add
 
         //Create n number of students
