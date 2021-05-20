ebe94148912a22a7adda974b554dbe499af776cc
alwinruby <alwinsolanky@gmail.com>
2021-05-20 10:34:23 +0100

    Student Database basics completed

--- src/studentdatabaseapp/StudentDataBaseApp.java
+++ src/studentdatabaseapp/StudentDataBaseApp.java
@@ -1,14 +1,27 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDataBaseApp {
     public static void main(String[] args){
 
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
+//        Student stu1 = new Student();
+//        stu1.enroll();
+//        stu1.payTuition();
+//        System.out.println(stu1.toString());
+
         //Ask how many users we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         //Create n number of students
+        for(int n = 0; n < numOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].toString());
+        }
     }
 }
